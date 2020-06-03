c5b73f975c6e788e9ec9eb0d8392d7b971a6eebb
danielbaker129 <baker.daniel129@gmail.com>
2020-06-03 16:27:12 -0400

    .

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -14,13 +14,13 @@ class Firebase {
 
   constructor() {
     var config = {
-      apiKey: process.env.GATSBY_REACT_APP_API_KEY,
-      authDomain: process.env.GATSBY_REACT_APP_AUTH_DOMAIN,
-      databaseURL: process.env.GATSBY_REACT_APP_DATABASE_URL,
-      projectId: process.env.GATSBY_REACT_APP_PROJECT_ID,
-      storageBucket: process.env.GATSBY_REACT_APP_STORAGE_BUCKET,
-      messagingSenderId: process.env.GATSBY_REACT_APP_SENDER_ID,
-      appId: process.env.GATSBY_REACT_APP_ID
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_DATABASE_URL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_SENDER_ID,
+      appId: process.env.REACT_APP_ID
     };
     app.initializeApp(config);
 
